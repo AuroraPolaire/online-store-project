c9350497a6d4810dcb0bdb35081df0d039427905
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-06-22 16:21:06 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div classNaeme="App">
       <div>test</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
